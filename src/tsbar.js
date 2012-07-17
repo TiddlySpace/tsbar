605609596dc254c5730f6d6d190d86e00cda43eb
Ben Gillies <bengillies@gmail.com>
2012-07-17 15:56:52 +0100

    Fleshed out the stub functions and added some tests
    Also added a .clear() function for removing widgets prior to creating
    a new toolbar.
    
    The tests should demonstrate how to use the API

--- src/tsbar.js
+++ src/tsbar.js
@@ -12,26 +12,62 @@
 	var tsbar = {};
 
 	// set up some private variables
-	var _widgets = {
+	var $bar, // the bar itself
+		_widgets = { // the widgets, sorted into where they will appear
 			left: [],
 			middle: [],
 			right: []
 		},
-		_defaultOptions = {
-			title: 'TiddlySpace',
-			defaultTemplate: [
+		_defaultOptions = { // some options that can be overridden by init
+			defaultTemplate: [ // the html that makes up the toolbar
 				'<div id="tsbar">',
 					'<div class="tsbar-left"></div>',
 					'<div class="tsbar-middle"></div>',
 					'<div class="tsbar-right"></div>',
 				'</div>'
-			].join()
+			].join('')
 		};
+
+	// set up some private functions
+
+	/*
+	 * return the container given by position
+	 */
+	function _fetchContainer(position) {
+		return $bar.find('.tsbar-' + position);
+	}
+
+	/*
+	 * Add the widget to the given container
+	 */
+	function _addWidget(widget, $container) {
+		$($container).append(widget);
+	}
 
 	/*
 	 * Provide a default widget that can be used to define buttons, etc
 	 */
 	tsbar.Widget = function(options) {
+		var _widget = {
+			el: '',
+			className: '',
+			popup: ''
+		};
+
+		$.extend(_widget, options);
+		var $el = $(_widget.el);
+
+		if (_widget.popup) {
+			$el.click(function(ev) {
+				ev.preventDefault();
+				$el.parent().toggleClass('open');
+			});
+		}
+
+		return $('<div/>', {
+				'class': 'tsbar-widget' + (_widget.className ?
+					' ' + _widget.className : '')
+			}).append($el).append(_widget.popup);
 	};
 
 	/*
@@ -40,6 +76,17 @@
 	 * position can be 'left', 'middle' or 'right'
 	 */
 	tsbar.define = function(position, widget) {
+		_widgets[position].push(widget);
+	};
+
+	/*
+	 * Remove all the pre-defined widgets ready for creating a new toolbar
+	 */
+	tsbar.clear = function() {
+		var key;
+		for (key in _widgets) if (_widgets.hasOwnProperty(key)) {
+			_widgets[key] = [];
+		}
 	};
 
 	/*
@@ -47,6 +94,22 @@
 	 * to the correct place
 	 */
 	tsbar.render = function(options) {
+		// override any default options
+		var defaults = $.extend({}, _defaultOptions);
+		$.extend(defaults, options);
+
+		// create the toolbar
+		$bar = $(defaults.defaultTemplate);
+
+		// add all the widgets to the toolbar
+		$.each(_widgets, function(position, widgets) {
+			var $container = _fetchContainer(position);
+			$.each(widgets, function(i, widget) {
+				_addWidget(widget, $container);
+			});
+		});
+
+		return $bar;
 	};
 
 	// export the bar to the global object
--- test/test_main.js
+++ test/test_main.js
@@ -1,4 +1,11 @@
-module('main bar');
+module('main bar', {
+	setup: function() {
+		tsbar.clear();
+	},
+	teardown: function() {
+		$('#sandbox').html();
+	}
+});
 
 test('tsbar complete', function() {
 	equal(typeof tsbar, 'object', 'tsbar exists');
@@ -6,4 +13,65 @@ test('tsbar complete', function() {
 	equal(typeof tsbar.define, 'function', 'you can publish widgets to the bar');
 	equal(typeof tsbar.render, 'function',
 		'there is a render function for choosing when to render');
+});
+
+test('tsbar.Widget returns a jQuery DOM element', function() {
+	var myWidget = tsbar.Widget();
+	strictEqual(myWidget.children().length, 0,
+		'blank Widget parameters lead to a widget with no children');
+
+	myWidget = tsbar.Widget({
+		el: '<span>Hello World.</span>',
+		popup: 'This is a popup.'
+	});
+	strictEqual(myWidget.text(), 'Hello World.This is a popup.',
+		'both elements have been appended to the widget');
+});
+
+test('tsbar.render renders any defined widgets', function() {
+	tsbar.define('left', tsbar.Widget({
+		el: '<span>Widget</span>',
+		popup: '<span>Popup</span>',
+		className: 'class-name'
+	}));
+
+	var toolbar = tsbar.render();
+
+	strictEqual(toolbar.find('.tsbar-widget.class-name').length, 1,
+		'the widget is in the toolbar');
+	strictEqual(toolbar.find('.tsbar-left').find('.class-name').length, 1,
+		'the widget is in the left section');
+});
+
+test('you can override the default template', function() {
+	var toolbar = tsbar.render();
+	strictEqual(toolbar.children().length, 3,
+		'there are three containers by default');
+
+	toolbar = tsbar.render({
+		defaultTemplate: '<div></div>'
+	});
+	strictEqual(toolbar.children().length, 0,
+		'you can override the default toolbar to whatever you want');
+});
+
+test('clicking on a widget toggles the "open" class', function() {
+	tsbar.define('left', tsbar.Widget({
+		el: '<button>Click me</button>',
+		popup: '<ul><li>list</li><li>of</li><li>items</li></ul>'
+	}));
+
+	var toolbar = tsbar.render();
+	$('#sandbox').append(toolbar);
+
+	strictEqual(toolbar.find('.open').length, 0,
+		'pre-clicking, there are no open widgets');
+
+	toolbar.find('.tsbar-widget button').click();
+	ok(toolbar.find('.tsbar-widget').hasClass('open'),
+		'the widget now has the open class');
+
+	toolbar.find('.tsbar-widget button').click();
+	strictEqual(toolbar.find('.open').length, 0,
+		'clicking again closes the widget');
 });
