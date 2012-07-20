59046d5c1b200dfe792439e05dd8cb55efd55ff7
Ben Gillies <bengillies@gmail.com>
2012-07-20 15:31:21 +0100

    Added correct info to the user popup
    The correct site icons, activity notifications, etc now appear.
    
    Still to do:
      - Add websocket support for notifications
      - hookup events (like login, edit SiteIcon, etc)
      - Add the notification count to the button

--- src/user.js
+++ src/user.js
@@ -6,23 +6,23 @@
 	/*
 	 * define a button template
 	 */
-	var button = [
+	var $button = $([
 		'<button class="tsbar-user-btn">',
 			'<img class="tsbar-user-icon">',
-			'<div class="tsbar-notification"></div>',
+			'<div class="tsbar-user-notification"></div>',
 		'</button>'
-	].join('');
+	].join(''));
 
 	/*
 	 * define a popup template
 	 */
-	var popup = [
+	var $popup = $([
 		'<div class="tsbar-popup">',
 			'<div class="tsbar-user-topbar">',
-				'<div class="tsbar-user-logged-out">'
+				'<div class="tsbar-logged-out">',
 					'You are not logged in',
 				'</div>',
-				'<div class="tsbar-user-logged-in">',
+				'<div class="tsbar-logged-in">',
 					'<button class="tsbar-logout">Log out</button>',
 				'</div>',
 			'</div>',
@@ -74,7 +74,239 @@
 					'<ul></ul>',
 					'<a href="#">See All</a>',
 				'</div>',
-			'</div>'
+			'</div>',
 		'</div>'
-	].join('');
+	].join(''));
+
+	/*
+	 * Define templates for the notification list
+	 */
+	var notificationTemplates = {
+		following: [
+			'<li>',
+				'<img class="tsbar-notification-icon">',
+				'<p><a class="tsbar-notification-user"></a> ',
+				'started following you</p>',
+			'</li>'].join(''),
+		atTag: [
+			'<li>',
+				'<img class="tsbar-notification-icon">',
+				'<p><a class="tsbar-notification-user"></a> ',
+				'wrote <a class="tsbar-notification-at"></a> for you</p>',
+			'</li>'].join('')
+	};
+
+	/*
+	 * fill in areas in HTML that require data from /status
+	 * hide areas that we don't need to see
+	 */
+	function populateUserDetails(status) {
+		var username = status.username,
+			siteIcon = 'http://tiddlyspace.com/bags/' + username +
+				'_public/tiddlers/SiteIcon',
+			loggedIn = username !== 'GUEST';
+
+		$button.find('img.tsbar-user-icon').attr('src', siteIcon);
+		$popup.find('img.tsbar-user-icon').attr('src', siteIcon);
+
+		$popup.find('.tsbar-logged-' + (loggedIn ? 'out' : 'in')).remove();
+
+		if (!loggedIn) {
+			$popup.find('.tsbar-login-openid').hide();
+		} else {
+			$popup.find('.tsbar-user-name').text(username);
+		}
+	}
+
+	/*
+	 * Get user details from status.js (or server)
+	 */
+	function getUserDetails(callback) {
+		if (window.tiddlyweb && tiddlyweb.status) {
+			callback(tiddlyweb.status);
+		} else {
+			$.getJSON('/status', callback);
+		}
+	}
+
+	/*
+	 * get the most recent few notifications and listen for new ones
+	 */
+	function getNotifications(username) {
+		var activity = new Activity({
+			username: username,
+			$countEl: $button.find('.tsbar-user-notification'),
+			$listEl: $popup.find('.tsbar-user-notification ul')
+		});
+
+		activity.getRecent(function(tiddlers) {
+			tiddlers.sort(function(a, b) { return b.modified - a.modified; });
+			activity.renderAll(tiddlers.slice(0, Activity.MAX_NOTIFICATIONS));
+		});
+	}
+
+	/*
+	 * add event listeners and behaviours to clicks inside the popup
+	 */
+	function hookupEvents() {
+	}
+
+
+	/*
+	 * Create an Activity object to handle notifications
+	 */
+	function Activity(options) {
+		Activity.MAX_NOTIFICATIONS = 7; // on Activity, not |this|
+
+		$.extend(this, options);
+
+		// set up a promise like system for lastChecked and followers
+		this.promises = {};
+
+		// get the time of the most recent notification the user saw
+		this.getLastChecked(function() {});
+	}
+
+	$.extend(Activity.prototype, {
+		getRecent: function(callback) {
+			var self = this;
+			this._promise('notifications', callback, function(doneFn) {
+				// get people who are now following you
+				$.ajax({
+					url: '/search?q=(title:@' + self.username +
+						'%20OR%20title:' + self.username +
+						')%20AND%20tag:follow&sort:-modified',
+					dataType: 'json',
+					success: _getRecentReplies,
+					error: _getRecentReplies
+				});
+
+				function _getRecentReplies(followers) {
+					var resultSet = [];
+					if ($.isArray(followers)) {
+						resultSet = followers;
+					}
+					var searchUrl = '/search?q=tag:@' + self.username +
+						'&sort=-modified';
+					$.ajax({
+						url: searchUrl,
+						dataType: 'json',
+						success: function(atReplies) {
+							doneFn(resultSet.concat(atReplies));
+						},
+						error: function() {
+							doneFn(resultSet);
+						}
+					});
+				}
+			});
+		},
+		render: function(tiddler) {
+			var self = this;
+
+			var template = tiddler.tags &&
+				~tiddler.tags.indexOf('@' + this.username) ?
+					notificationTemplates.atTag :
+					notificationTemplates.following;
+
+			var $li = $(template).find('.tsbar-notification-icon')
+					.attr('src', 'http://tiddlyspace.com/bags/' + tiddler.bag +
+						'_public/tiddlers/SiteIcon')
+				.end().find('.tsbar-notification-user')
+					.text(tiddler.modifier)
+					.attr('href', 'http://' + tiddler.modifier +
+						'.tiddlyspace.com')
+				.end().find('.tsbar-notification-at')
+					.attr('href', tiddler.url).text(tiddler.title).end();
+
+			this.getLastChecked(function(lastChecked) {
+				if (_getDate(tiddler.modified) > lastChecked) {
+					$li.addClass('tsbar-notification-new');
+				}
+				self.$listEl.prepend($li);
+			});
+		},
+		renderAll: function(tiddlers) {
+			var self = this;
+
+			tiddlers = tiddlers.slice(0, this.MAX_NOTIFICATIONS);
+
+			$.each(tiddlers, function(i, tiddler) { self.render(tiddler); });
+		},
+		getLastChecked: function(callback) {
+			var self = this;
+			this._promise('lastChecked', callback, function(doneFn) {
+				$.ajax({
+					url: '/bags/' + self.username + '_private/tiddlers/' +
+						'_notificationState',
+					dataType: 'json',
+					success: function(tiddler) {
+						doneFn(_getDate(tiddler.modified));
+					},
+					error: function() {
+						doneFn(0);
+					}
+				});
+			});
+		},
+		listen: function() {
+		},
+		_promise: function(name, callback, promise) {
+			var self = this;
+			if (name in this) {
+				callback(this[name]);
+			} else {
+				if (!this.promises[name]) {
+					this.promises[name] = [callback];
+				} else {
+					this.promises[name].push(callback);
+				}
+				promise(function(data) {
+					self[name] = data;
+					$.each(self.promises[name], function(i, fn) {
+						fn(data);
+					});
+				});
+			}
+		}
+	});
+
+	/*
+	 * Turn a TiddlyWeb date into a date object
+	 */
+	function _getDate(date) {
+		return new Date(Date.UTC(
+			parseInt(date.substr(0, 4), 10),
+			parseInt(date.substr(4, 2), 10) - 1,
+			parseInt(date.substr(6, 2), 10),
+			parseInt(date.substr(8, 2), 10),
+			parseInt(date.substr(10, 2), 10),
+			parseInt(date.substr(12, 2) || "0", 10),
+			parseInt(date.substr(14, 3) || "0", 10)
+			));
+	}
+
+
+	/*
+	 * Create the widget and register it
+	 */
+	function main() {
+		getUserDetails(function(status) {
+			populateUserDetails(status);
+			if (status.username) {
+				getNotifications(status.username);
+			}
+			hookupEvents();
+
+			tsbar.userWidget = tsbar.Widget({
+				el: $button,
+				popup: $popup
+			});
+		});
+	}
+
+
+
+	main();
+
 }(jQuery));
