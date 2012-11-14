4034135c801f057fb1c891a36f07353221373755
Ben Paddock <pads@thisispads.me.uk>
2012-11-14 16:01:44 +0000

    Refactor - slightly more elegant way of splitting the template
    Splitting the template into the button element and the popup.

--- src/search.js
+++ src/search.js
@@ -7,9 +7,8 @@ tsbar.initSearchWidget = function(exports, $) {
     function SearchWidget() {
 
         this.$compiledTemplate = $(exports.tswidgets.templates['search.hbs'](Handlebars));
-        //TODO: this is a hack as the pre-compiled templates have carriage returns which confuses jquery
-        this.$el = $(this.$compiledTemplate[0]);
-        this.$popup = $(this.$compiledTemplate[2]);
+        this.$el = this.$compiledTemplate.first();
+        this.$popup = this.$compiledTemplate.last();
 
         this._widget = tsbar.Widget({
             el: this.$el,
