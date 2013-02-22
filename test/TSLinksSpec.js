73a2a442eb8edc981f3cdddc0cea0b2505d1d7cc
Colm Britton <colmjude@gmail.com>
2013-02-22 16:35:23 +0000

    remove old describe block from tests

--- test/TSLinksSpec.js
+++ test/TSLinksSpec.js
@@ -36,16 +36,4 @@ describe('TS Links Widget', function() {
 		});
 	});
 
-	// describe('when rendered', function() {
-	// 	tsLinksWidget = tsbar.tsLinksWidget;
-	// 	
-	// 	it('should contain a button', function() {
-	// 		expect(tsLinksWidget.find(".tsbar-links-btn").length).toEqual(1);
-	//         });
-	// 
-	// 	it('should contain a popup with an iframe', function() {
-	// 		expect(tsLinksWidget.find(".tsbar-links-widget").length).toEqual(1);
-	// 	});
-	// })
-
 });
